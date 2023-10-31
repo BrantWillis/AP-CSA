bce2b86f5975f7884dd92c6aa22c3226319a7e8b
Brant Willis <brantwillis@amdg.rockhursths.edu>
2023-10-31 12:25:55 -0500

    vector

--- IntVector.java
+++ IntVector.java
@@ -14,4 +14,73 @@ public class IntVector {
     }
 
     public int size() { return size; }
+
+    private void testBounds(int index) {
+        if(index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private void tryResize() {
+        if(size >= arr.length - 1) {
+            int[] temp = new int[1 + arr.length * 2];
+
+            for (int i = 0; i < size; i++) {
+                temp[i] = arr[i];
+            }
+
+            arr = temp;
+        }
+    }
+
+    public void add(int value) {
+        tryResize();
+        arr[size++] = value;
+    }
+
+    public boolean add(int index, int value) {
+        testBounds(index);
+        int temp = arr[index];
+        arr[index] = value;
+        add(arr[size - 1]);
+        for (int i = size - 2; i > index; i--) {
+            arr[i+1] = arr[i];
+        }
+        arr[index + 1] = temp;
+        if (arr[index] == value) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean remove(int value) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == value) {
+                for (int j = i; j < size - 1; j++) {
+                    arr[j] = arr[j+1];
+                }
+                size--;
+                tryResize();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void set(int index, int value) {
+        testBounds(index);
+        arr[index] = value;
+    }
+
+    @Override
+    public String toString() {
+        if(size == 0) {
+            return "[]";
+        }
+        String t = "[" + arr[0];
+        for(int i = 1; i < size; i++) {
+            t += ", " + arr[i];
+        }
+        return t + "]";
+    }
 }
--- Start.java
+++ Start.java
@@ -1,13 +1,24 @@
 public class Start {
     public static void main(String[] args) {
         try {
-            IntVector test = new IntVector(-1);
-    
-            System.out.println("Size: " + test.size());
+            IntVector test = new IntVector(0);
             
-            for (int i = 0; i < test.size(); i++) {
-    
+            for (int i = 0; i < 20; i++) {
+                test.add(i*2);
             }
+            System.out.println(test);
+
+            test.add(17,100);
+            System.out.println(test);
+
+            //System.out.println(test);
+
+            //test.add(50,50);
+            System.out.println(test);
+
+            //test.remove(3);
+            //System.out.println(test);
+            
         } catch(IllegalArgumentException ex) {
             System.out.println("That's illegal!");
         } catch(ArithmeticException ex) {
