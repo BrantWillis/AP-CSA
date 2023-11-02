836f5a2eb40b2d901b662f50f7b2a03a20001709
Brant Willis <brantwillis@amdg.rockhursths.edu>
2023-11-02 12:34:47 -0500

    nov 2, second add

--- LinearStructures/ArrayList.java
+++ LinearStructures/ArrayList.java
@@ -43,7 +43,100 @@ public class ArrayList<E> {
         size++;
     }
 
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void checkIndex(int index) {
+        if(index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("index " + index + " out of bounds");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void clear() {
+        data = (E[])new Object[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    public boolean contains(E e) {
+        for(int i = 0; i < size; i++) {
+            if(e.equals(data[i])) return true;
+        }
+        return false;
+    }
+
+    public E get(int index) {
+        checkIndex(index);
+        return data[index];
+    }
+
+    public int indexOf(E e) {
+        for(int i = 0; i < size; i++) {
+            if(e.equals(data[i])) return i;
+        }
+        return -1;
+    }
+    
+    public int lastIndexOf(E e) {
+        for(int i = size - 1; i >= 0; i--) {
+            if (e.equals(data[i])) return i;
+        }
+        return -1;
+    }
+
+    public E remove(int index) {
+        checkIndex(index);
+        E e = data[index];
+        for(int j = index; j < size; j++) {
+            data[j] = data[j+1]; // shift values left, overwrites original
+        }
+        data[size-1] = null; // erase last index
+        size--;
+        return e; // return original
+    }
+
+    // removes the first value found
+    public boolean remove(E e) {
+        if (indexOf(e) >= 0) {
+            remove(indexOf(e));
+            return true;
+        }
+        return false;
+    }
+
+    public E set(int index, E e) {
+        checkIndex(index);
+        E old = data[index];
+        data[index] = e;
+        return old;
+    }
+
+    @SuppressWarnings("unchecked")
+    public void trimToSize() {
+        if (size != data.length) {
+            E[] newData = (E[])new Object[size];
+            System.arraycopy(data, 0, newData, 0, size);
+            data = newData;
+        }
+    }
+
     @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("[");
+        for(int i = 0; i < size; i++) {
+            result.append(data[i]);
+            if(i < size - 1) result.append(", ");
+        }
+        result.append("]");
+        return result.toString();
+    }
+
+    /*@Override
     public String toString() {
         if(size == 0) {
             return "[]";
@@ -53,5 +146,5 @@ public class ArrayList<E> {
             t += ", " + data[i];
         }
         return t + "]";
-    }
+    }*/
 }
--- Start.java
+++ Start.java
@@ -7,11 +7,26 @@ public class Start {
 
         strArr.add("hi");
         strArr.add("yeah");
+        strArr.add("brant");
         strArr.add(0,"georgie");
+        strArr.add("georgie");
+        strArr.add("georgie");
+
+        System.out.println(strArr);
+
+        while (strArr.remove("georgie") == true) {}
+        strArr.set(0, "hello");
 
         intArr.add(5);
         intArr.add(7);
         intArr.add(0, 1);
+        intArr.add(56);
+        intArr.add(73);
+        intArr.add(56);
+        
+        System.out.println(intArr);
+
+        intArr.remove((Integer)56);
 
 
 
